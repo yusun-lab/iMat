41a84a76b4bc1b4fbbfb94684d99f6668fe03a38
yusun-lab <yu.sun@hyperisland.se>
2025-08-07 11:42:27 +0200

    Routes created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import NavBar from './components/NavBar/NavBar'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div className='app'>
       <NavBar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/order' element={<PlaceOrder/>} />
+      </Routes>
     </div>
   )
 }
---
+++ Frontend/src/pages/Cart/Cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.css'
+
+const Cart = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Cart
