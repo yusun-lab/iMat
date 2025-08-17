a0598215564374ff6cb9663a906bfc4175c5cfc0
yusun-lab <yu.sun@hyperisland.se>
2025-08-17 21:57:33 +0200

    Optimize code structure in index.css, ExploreMenu.css, Header.css, and NavBar,css;     change main orange to darkorange;     rename assets.js to assets.jsx;     create FoodItem component;     add style for FoodDisplay and FoodItem components;     add logo, search and cart icons; create PlusIcon and MinusIcon components.

--- Frontend/src/components/FoodItem/FoodItem.jsx
+++ Frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,22 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './FoodItem.css' 
 import { assets } from '../../assets/assets'
+import { PlusIcon } from '../../assets/assets'
+import { MinusIcon } from '../../assets/assets'
+
 
 const FoodItem = ({id, name, price, description, image}) => {
+
+  const [itemCount, setItemCount] = useState(0);
+
   return (
     <div className='food-item'>
       <div className='food-item-image-container'>
-        <img src={image} alt="" className="food-item-image" />
+        <img src={image} alt={name} className='food-item-image' />
+        {!itemCount
+          ? <PlusIcon 
+              onClick={() => setItemCount(prev => prev+1)} 
+              className='black-plus-icon'
+            />
+          : <div className='food-item-counter'>
+              <MinusIcon 
+                onClick={() => setItemCount(prev => prev-1)} 
+              />
+              <p>{itemCount}</p>
+              <PlusIcon 
+                onClick={() => setItemCount(prev => prev+1)} 
+                color="green"
+                className='green-plus-icon'
+              />
+            </div>
+        }
       </div>
+
       <div className="food-item-info">
-        <div className="food-item-name-rating">
-          <p>{name}</p>
-          <img src={assets.rating_starts} alt="" />
-        </div>
+        <p>{name}</p>
+        <img src={assets.rating_starts} alt="" />
         <p className='food-item-desc'>{description}</p>
-        <p className='food-item-price'>{price} SEK</p>
+        <p className='food-item-price'>SEK {price}</p>
       </div>
-      
     </div>
   )
 }
