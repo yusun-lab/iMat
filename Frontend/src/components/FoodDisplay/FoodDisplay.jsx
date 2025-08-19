6ebae65951cccf23dbdb3ceb15359a7fc19f6d99
yusun-lab <yu.sun@hyperisland.se>
2025-08-19 17:52:19 +0200

    Refactor FoodDisplay: destructure props and add category-based filtering before mapping

--- Frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ Frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -3,17 +3,29 @@ import './FoodDisplay.css'
 import { StoreContext } from '../../context/StoreContext'
 import FoodItem from '../FoodItem/FoodItem'
 
-const FoodDisplay = (category) => {
+const FoodDisplay = ({category}) => {
 
   const {food_list} = useContext(StoreContext)
+
+  const visibleFoods = food_list.filter(item => 
+    category === 'All' || category === item.category
+  );
 
   return (
     <div className='food-display' id='food-display'>
       <h2>Top dishes near you</h2>
-      <div className="food-display-list">
-        {food_list.map((item, index) => {
-          return <FoodItem key={index} id={item.id} name={item.name} price={item.price} description={item.description} image={item.image} />
-        })}
+      
+       <div className="food-display-list">
+        {visibleFoods.map(item => (
+          <FoodItem
+            key={item.id} 
+            id={item.id} 
+            name={item.name}
+            price={item.price}
+            description={item.description}
+            image={item.image}
+          />
+        ))}
       </div>
     </div>
   )
