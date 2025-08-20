c70939d658cbb7549b7706b472c409c6c6d6e756
yusun-lab <yu.sun@hyperisland.se>
2025-08-21 00:23:05 +0200

    - Create Footer.jsx and Footer.css - Add company logo, brief description, and social media icons - Add company links section and contact info - Include copyright notice

---
+++ Frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import './Footer.css'
+import { assets, TwitterIcon, YoutubeIcon, InstagramIcon, FacebookIcon } from '../../assets/assets' 
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="logo" />
+          <p>We make food ordering simple and enjoyable for everyone.</p>
+          <div className="footer-social-icons">
+            <YoutubeIcon className='youtube-icon' />
+            <TwitterIcon className='twitter-icon' />
+            <InstagramIcon className='instagram-icon' />
+            <FacebookIcon className='facebook-icon' />
+          </div>
+        </div>
+        
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+46123456789</li>
+            <li>contact@imat.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className='footer-copyright'>Copyright 2025 © imat.com - All Right Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer 
