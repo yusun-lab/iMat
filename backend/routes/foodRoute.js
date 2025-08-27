f87e794392f5f210af37369dc7de059279930c6c
yusun-lab <yu.sun@hyperisland.se>
2025-08-27 12:03:02 +0200

    feat(admin): initialize admin module chore: setup Prettier

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,5 +1,9 @@
 import express from "express";
-import { addFood, foodList, deleteFood } from "../controllers/foodController.js";
+import {
+  addFood,
+  foodList,
+  deleteFood,
+} from "../controllers/foodController.js";
 import multer from "multer";
 
 const foodRouter = express.Router();
@@ -9,21 +13,16 @@ const foodRouter = express.Router();
 const storage = multer.diskStorage({
   destination: "uploads/",
   filename: (req, file, cb) => {
-    return cb(null, `${Date.now()}${file.originalname}`)
-  }
-})
+    return cb(null, `${Date.now()}${file.originalname}`);
+  },
+});
 
-const upload = multer({ storage: storage })
+const upload = multer({ storage: storage });
 
-foodRouter.post("/add", upload.single("image"), addFood)
+foodRouter.post("/add", upload.single("image"), addFood);
 
-foodRouter.get("/list", foodList)
+foodRouter.get("/list", foodList);
 
-foodRouter.delete("/delete/", deleteFood)
-
-
-
-
- 
+foodRouter.delete("/delete/", deleteFood);
 
 export default foodRouter;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
-import express from 'express';
-import cors from 'cors';
-import { connectDB } from './config/db.js';
-import foodRouter from './routes/foodRoute.js';
-
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
 
 // app fonfig
 const app = express();
@@ -16,13 +15,13 @@ app.use(cors());
 connectDB();
 
 // api endpoints
-app.use('/api/food', foodRouter)
-app.use('/images', express.static('uploads'));
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-})
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
-})
+});
